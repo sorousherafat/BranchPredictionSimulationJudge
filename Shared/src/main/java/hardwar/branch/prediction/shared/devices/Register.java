9569a9f32c954fd1a7de24d106c091bbf8c38a42
Soroush Sherafat <sorousherafat@gmail.com>
2023-05-20 00:49:57 +0330

    Shared: added `Register` interface

---
+++ Shared/src/main/java/hardwar/branch/prediction/shared/devices/Register.java
@@ -0,0 +1,16 @@
+package hardwar.branch.prediction.shared.devices;
+
+import hardwar.branch.prediction.shared.Bit;
+import hardwar.branch.prediction.shared.Monitorable;
+
+public interface Register extends Monitorable {
+    Bit[] read();
+
+    void load(Bit[] bits);
+
+    void insert(Bit bit);
+
+    int getLength();
+
+    void clear();
+}
