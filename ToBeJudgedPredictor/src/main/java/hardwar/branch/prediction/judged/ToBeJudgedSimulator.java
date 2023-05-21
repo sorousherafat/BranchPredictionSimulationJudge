e6211def11c10a16cdf77d0878010933d95746e9
Soroush Sherafat <sorousherafat@gmail.com>
2023-05-22 02:34:22 +0330

    ToBeJudged: added `BranchInstruction` type

--- ToBeJudgedPredictor/src/main/java/hardwar/branch/prediction/judged/ToBeJudgedSimulator.java
+++ ToBeJudgedPredictor/src/main/java/hardwar/branch/prediction/judged/ToBeJudgedSimulator.java
@@ -1,6 +1,7 @@
 package hardwar.branch.prediction.judged;
 
 import hardwar.branch.prediction.shared.Bit;
+import hardwar.branch.prediction.shared.BranchInstruction;
 import hardwar.branch.prediction.shared.BranchPredictor;
 import hardwar.branch.prediction.shared.BranchResult;
 
@@ -8,12 +9,12 @@ public class ToBeJudgedSimulator implements BranchPredictor {
     private static final String NOT_IMPLEMENTED_MESSAGE = "Replace this dummy class with the one you want to test.";
 
     @Override
-    public BranchResult predict(Bit[] bits) {
+    public BranchResult predict(BranchInstruction instruction) {
         throw new UnsupportedOperationException(NOT_IMPLEMENTED_MESSAGE);
     }
 
     @Override
-    public void update(Bit[] bits, BranchResult branchResult) {
+    public void update(BranchInstruction instruction, BranchResult result) {
         throw new UnsupportedOperationException(NOT_IMPLEMENTED_MESSAGE);
     }
 
