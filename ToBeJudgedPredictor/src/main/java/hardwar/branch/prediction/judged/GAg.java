9fdfa384c4bf5900cd788879df3b06331c4fcb44
halfblood <arashyadegari0402@gmail.com>
2023-05-29 15:55:07 +0330

    replace multitask register model with combination of logic and register

--- ToBeJudgedPredictor/src/main/java/hardwar/branch/prediction/judged/GAg.java
+++ ToBeJudgedPredictor/src/main/java/hardwar/branch/prediction/judged/GAg.java
@@ -1,34 +1,32 @@
 package hardwar.branch.prediction.judged;
 
-import hardwar.branch.prediction.shared.Bit;
-import hardwar.branch.prediction.shared.BranchInstruction;
-import hardwar.branch.prediction.shared.BranchPredictor;
-import hardwar.branch.prediction.shared.BranchResult;
+import hardwar.branch.prediction.shared.*;
 import hardwar.branch.prediction.shared.devices.*;
 
 import java.util.Arrays;
 
 public class GAg implements BranchPredictor {
 
-    private final Register BHR; // branch history register
-    private final Register SC; // saturating counter
+    private final ShiftRegister BHR; // branch history register
     private final Cache<Bit[], Bit[]> PHT; // page history table
+
+    private final ShiftRegister SC; // saturated counter value
 
     /**
      * Creates a new GAg predictor with the given BHR register size and initializes the BHR and PHT.
      *
      * @param BHRSize the size of the BHR register
-     * @param SCSize  the size of the saturating counter
+     * @param SCSize  the size of the register which hold the saturating counter value
      */
     public GAg(int BHRSize, int SCSize) {
         // Initialize the BHR register with the given size and null input
-        this.BHR = new SerialInParallelOutRegister("bhr", BHRSize, null);
+        this.BHR = new SIPORegister("bhr", BHRSize, null);
 
         // Initialize the PHT with a size of 2^size and each entry having a saturating counter of size "size"
         PHT = new PageHistoryTable((int) Math.pow(2, BHRSize), SCSize);
 
-        // Initialize the saturating counter
-        SC = new SaturatingCounter(SCSize, null);
+        // Initialize the SC register
+        SC = new SIPORegister("sc", SCSize, null);
     }
 
     /**
@@ -45,7 +43,7 @@ public class GAg implements BranchPredictor {
         // Get the associated block with the current value of the BHR register from the PHT
         Bit[] cacheBlock = PHT.setDefault(BHRValue, getDefaultBlock());
 
-        // load the block into the counter
+        // load the block into the register
         SC.load(cacheBlock);
 
         // Return the predicted outcome of the branch instruction based on the value of the MSB
@@ -60,14 +58,17 @@ public class GAg implements BranchPredictor {
      */
     @Override
     public void update(BranchInstruction instruction, BranchResult actual) {
+        // get the cache value
+
+
         // check the predication result
         boolean isTaken = actual == BranchResult.TAKEN;
 
         // update saturating counter
-        SC.insert(isTaken ? Bit.ONE : Bit.ZERO);
+        Bit[] nValue = CombinationalLogic.count(SC.read(), isTaken, CountMode.SATURATING);
 
         // add updated value to the cache
-        PHT.put(BHR.read(), SC.read());
+        PHT.put(BHR.read(), nValue);
 
         // update global history
         BHR.insert(isTaken ? Bit.ONE : Bit.ZERO);
@@ -95,14 +96,29 @@ public class GAg implements BranchPredictor {
     }
 
 
+    /*
+    Test Util
+
+    TODO: remove this section in final result
+     */
     public static void main(String[] args) {
         GAg gag = new GAg(4, 2);
 
+        Bit[] opcode;
+        Bit[] instructionAddress;
+        Bit[] jumpAddress;
+
 
         for (int i = 0; i < 1000; i++) {
-            BranchInstruction bi = new BranchInstruction(getRandomBitSerial(6),
-                    getRandomBitSerial(6),
-                    getRandomBitSerial(4)
+
+            opcode = getRandomBitSerial(6);
+            instructionAddress = getRandomBitSerial(10);
+            jumpAddress = getRandomBitSerial(16);
+
+            BranchInstruction bi = new BranchInstruction(
+                    opcode,
+                    instructionAddress,
+                    jumpAddress
             );
 
             BranchResult br = getRandomBR();
