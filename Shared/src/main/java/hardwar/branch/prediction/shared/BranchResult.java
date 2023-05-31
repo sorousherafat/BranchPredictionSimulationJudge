8f660d6af9f854bcb7d3238d4bb473932446891d
Soroush Sherafat <sorousherafat@gmail.com>
2023-05-31 04:19:11 +0330

    Shared: add factory method to `BranchResult`

--- Shared/src/main/java/hardwar/branch/prediction/shared/BranchResult.java
+++ Shared/src/main/java/hardwar/branch/prediction/shared/BranchResult.java
@@ -4,6 +4,10 @@ public enum BranchResult {
     TAKEN,
     NOT_TAKEN;
 
+    public static BranchResult of(boolean taken) {
+        return taken ? TAKEN : NOT_TAKEN;
+    }
+
     public static boolean isTaken(BranchResult branchResult) {
         return branchResult == TAKEN;
     }
