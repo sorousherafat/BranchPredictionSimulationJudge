1d8ea894c6b868bc7c34fc81d63d5dfa411d48a4
Soroush Sherafat <sorousherafat@gmail.com>
2023-05-31 05:23:05 +0330

    WorkloadGenerator: added `WorkloadTester` type

---
+++ WorkloadGenerator/src/main/java/hardwar/branch/prediction/workload/WorkloadTester.java
@@ -0,0 +1,23 @@
+package hardwar.branch.prediction.workload;
+
+import java.util.List;
+
+public class WorkloadTester {
+    private final List<String> predictorNames;
+
+    private final String basePredictorName;
+
+    private final PredictorComparator comparator;
+
+    public WorkloadTester(List<String> predictorNames, String basePredictorName) {
+        this.predictorNames = predictorNames;
+        this.basePredictorName = basePredictorName;
+        this.comparator = new PredictorComparator(predictorNames);
+    }
+
+    public boolean test(Workload workload) {
+        List<String> comparisonResult = comparator.compare(workload);
+        String bestPredictorName = comparisonResult.get(0);
+        return bestPredictorName.equals(basePredictorName);
+    }
+}
